b0b4c4b62ef0ecf4082de984d9d4573954d05c73
Atakan Celebi <atakancelebi2007@gmail.com>
2025-02-27 18:31:24 +0100

    Add project files.

---
+++ Conversione Temperatura C sharp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("inserisc i gradi in fahrenheit");
+float F = float.Parse(Console.ReadLine());
+Console.WriteLine("i gradi in celsius sono: {0}",((F-32)*5)/9);
